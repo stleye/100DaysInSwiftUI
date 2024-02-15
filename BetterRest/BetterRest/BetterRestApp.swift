6b0e9be21836bb35a2f7054a899539529e1bfe07
Sebastian Tleye <stleye@gmail.com>
2024-02-14 21:57:36 -0300

    Added BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sebastian Tleye on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

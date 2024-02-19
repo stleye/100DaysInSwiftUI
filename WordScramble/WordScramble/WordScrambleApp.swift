0857b2a6e0de6b330b86551f83b409b7a0f5c13d
Sebastian Tleye <stleye@gmail.com>
2024-02-19 14:36:57 -0300

    Added WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sebastian Tleye on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
